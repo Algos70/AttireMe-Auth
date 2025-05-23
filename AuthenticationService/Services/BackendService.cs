af73e6975a4441cd1b95ac10736be5839947f1ec
Kürşat <draknes.gamin70@gmail.com>
2025-05-23 17:53:15 +0300

    remove add bearer to auth header bug

--- AuthenticationService/Services/BackendService.cs
+++ AuthenticationService/Services/BackendService.cs
@@ -25,7 +25,7 @@ public class BackendService : IBackendService
     {
         try
         {
-            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", adminToken);
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(adminToken);
             
             var requestData = new 
             { 
